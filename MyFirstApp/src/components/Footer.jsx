f65e323ca752232c092288a854c811f4290370f6
R.Smith <rhiannacsmith@hotmail.com>
2024-03-12 21:55:34 -0600

    Lab 5
    Added Home and About pages

---
+++ MyFirstApp/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    SafeAreaView,
+    StyleSheet,
+    Pressable,
+    View,
+    Text,
+    ScrollView,
+    TextInput,
+    Button
+  } from 'react-native';
+
+export default function Footer() {
+    return (
+        <SafeAreaView>
+        <Text>Lab 5</Text>
+        </SafeAreaView>
+    );
+    }
